c8df851d4369eeb8367f685c4f5f6616a1b4bfd4
MIrvanAminudin <irvan21xtkj1@gmail.com>
2016-12-05 09:35:52 +0700

    M.IRVAN

---
+++ TugasKt.ino
@@ -0,0 +1,21 @@
+int LDR= A2;          
+int LED= 13;          
+int nilaiLDR= 0;  
+void setup()
+{
+  pinMode(LED, OUTPUT);          
+  Serial.begin(9600);          
+}
+void loop()
+{
+  nilaiLDR= analogRead(LDR);  
+  Serial.print("NilaiLDR= ");         
+  Serial.println(nilaiLDR);                 
+  if(nilaiLDR < 1000) {          
+    digitalWrite(LED, HIGH);         
+  }
+  else  
+  {                           
+    digitalWrite(LED, LOW);         
+  }
+}
